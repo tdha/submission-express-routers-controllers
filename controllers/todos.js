ff77052502f968b099a0733b6ae1d73e81d7cd23
Ton Ha <ton@tdha.co>
2024-02-21 11:27:35 +1000

    Commit before remote to Jim's

--- controllers/todos.js
+++ controllers/todos.js
@@ -5,14 +5,15 @@ module.exports = {
     show
 };
 
-function show(req, res) {
-    res.render('todos/show', {
-        todo: Todo.getOne(req.params.id)
-    });
-}
 
 function index(req, res) {
     res.render('todos/index', {
       todos: Todo.getAll()
     });
-  }
+}
+
+function show(req, res) {
+    res.render('todos/show', {
+     todo: Todo.getOne(req.params.id)
+    });
+}
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// the first arg is the 'starts with' path
+// the first arg is the 'starts with' path (prefix)
 // the paths within the route modules are appended to the 'starts with paths'
 app.use('/', indexRouter);
 app.use('/todos', todosRouter);
